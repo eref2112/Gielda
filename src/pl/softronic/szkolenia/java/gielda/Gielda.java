d17d6a2db43567eb15cd2e0cd303d872eb9e639e
eref2112 <erfi2112@gmail.com>
2021-11-25 10:47:10 +0100

    kolejne

--- src/pl/softronic/szkolenia/java/gielda/Gielda.java
+++ src/pl/softronic/szkolenia/java/gielda/Gielda.java
@@ -1,13 +1,27 @@
 package pl.softronic.szkolenia.java.gielda;
 
 public class Gielda {
-    String nazwa = "";
-    String adres = "";
+    private String nazwa = "";
+    protected String adres = "";
 
     public static void main(String[] args) {
         Kontroler kontroler = new Kontroler();
         kontroler.uruchomProgram();
     }
 
+    public String getNazwa() {
+        return nazwa;
+    }
 
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
 }
--- src/pl/softronic/szkolenia/java/gielda/Kontroler.java
+++ src/pl/softronic/szkolenia/java/gielda/Kontroler.java
@@ -1,24 +1,31 @@
 package pl.softronic.szkolenia.java.gielda;
 
 public class Kontroler {
+    Gielda gielda;
+
     public void uruchomProgram() {
         //Uruchamia metody: tworzącą i testujacą giełdę
-
         tworzGielde();
         wczytajDane();
         testujGielde();
     }
 
-    private void wczytajDane() {
-
-    }
-
     public void tworzGielde() {
-
         System.out.println("Tworzę giełdę");
+
+        //Tworzymy giełdę
+        gielda = new Gielda();
+        gielda.setNazwa("Giełda wspaniałych pojazdów");
+        gielda.adres = "Warszawa ul. Jasna ";
+
     }
 
     public void testujGielde(){
+
         System.out.println("Testuję giełdę");
+    }
+
+    private void wczytajDane() {
+
     }
 }
